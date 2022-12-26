b9bd8955c0467f0b6c5e8680a95fa9db4734bb17
yyeongseok <yeongseok0219@gmail.com>
2022-12-26 18:42:13 +0900

    [40기 김영석]ADD: Initial-setting 초기세팅

---
+++ app.js
@@ -0,0 +1,19 @@
+const express =  require ('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const routes = require('./api/routes');
+
+const createApp = () =>{
+    const app = express();
+
+    app.use(express.json());
+    app.use(cors());
+    app.use(morgan('combined'));
+
+    app.use(routes);
+    
+    return app;
+};
+
+module.exports = { createApp }
